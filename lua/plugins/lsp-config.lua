01d149929938fbd0709bdea4f5383309bbbf7171
bigpigmooon <matvey.novikov02@gmail.com>
2023-12-31 06:42:21 +0700

    feat: add autocomplition windows wow!

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,16 +9,25 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
+				ensure_installed = { "lua_ls", "tsserver", "html" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
+			lspconfig.lua_ls.setup({
+                capabilites = capabilities,
+            })
+			lspconfig.tsserver.setup({
+                capabilites = capabilities,
+            })
+            lspconfig.html.setup({
+                capabilites = capabilities,
+            })
 
 			local opts = {}
 
