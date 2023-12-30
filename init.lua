c9fd7d4e1464fc784f738932c815855888a13fa7
bigpigmooon <matvey.novikov02@gmail.com>
2023-12-31 03:59:29 +0700

    feat: packer manage, colorscheme, fzf, some mappings

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- syntax enable                     -- Включить подсветку синтаксиса
+vim.cmd("syntax enable")
 vim.cmd("set number")                -- Отображать номера строк
 vim.cmd("set relativenumber")        -- Отображать относительные номера строк
 vim.cmd("set showmatch")             -- Подсветка парных скобок
@@ -24,3 +24,38 @@ vim.cmd("set expandtab")             -- Использовать пробелы 
 vim.cmd("set nowrap")                -- Не переносить строки
 vim.cmd("set encoding=utf-8")        -- Кодировка UTF-8
 vim.cmd("set noswapfile")            -- Не создает резервные файлы
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+     'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>qq', ":q!<CR>", {})
+
+require("catppuccin").setup()
+
+vim.cmd.colorscheme "catppuccin"
